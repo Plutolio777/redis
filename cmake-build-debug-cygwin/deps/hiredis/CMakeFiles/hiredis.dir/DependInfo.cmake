c047f42cc7266bb12ac851fbbff8d4e5187f8a41
Plutolio777 <530638701@qq.com>
2023-10-07 22:16:18 +0800

    redis本地编译

---
+++ cmake-build-debug-cygwin/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cygdrive/d/lyj/redis/deps/hiredis/async.c" "deps/hiredis/CMakeFiles/hiredis.dir/async.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/async.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/dict.c" "deps/hiredis/CMakeFiles/hiredis.dir/dict.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/dict.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/hiredis.c" "deps/hiredis/CMakeFiles/hiredis.dir/hiredis.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/hiredis.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/net.c" "deps/hiredis/CMakeFiles/hiredis.dir/net.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/net.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/read.c" "deps/hiredis/CMakeFiles/hiredis.dir/read.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/read.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/sds.c" "deps/hiredis/CMakeFiles/hiredis.dir/sds.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/sds.c.o.d"
+  "/cygdrive/d/lyj/redis/deps/hiredis/sockcompat.c" "deps/hiredis/CMakeFiles/hiredis.dir/sockcompat.c.o" "gcc" "deps/hiredis/CMakeFiles/hiredis.dir/sockcompat.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-cygwin/deps/hiredis/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/d/lyj/redis/deps/hiredis
+# Build directory: /cygdrive/d/lyj/redis/cmake-build-debug-cygwin/deps/hiredis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "/cygdrive/d/lyj/redis/deps/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/d/lyj/redis/deps/hiredis/CMakeLists.txt;157;ADD_TEST;/cygdrive/d/lyj/redis/deps/hiredis/CMakeLists.txt;0;")
