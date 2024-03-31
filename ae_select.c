b44e9cd73c28ec2addb177d21f0f9fb717d645bc
Plutolio777 <530638701@qq.com>
2024-04-01 00:27:00 +0800

    redis-cli源码学习[2024.04.01]

--- ae_select.c
+++ ae_select.c
@@ -3,7 +3,7 @@
  * Released under the BSD license. See the COPYING file for more info. */
 
 #include <string.h>
-
+#include <sys/select.h>
 typedef struct aeApiState {
     fd_set rfds, wfds;
     /* We need to have a copy of the fd sets as it's not safe to reuse
